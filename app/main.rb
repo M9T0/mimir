fc3e5649be39d4906fa663f4ecd12a6a97045078
Makoto Ito <millionlives@gmail.com>
2017-08-24 12:06:27 +0900

    display rss.

--- app/main.rb
+++ app/main.rb
@@ -1,14 +1,23 @@
 #coding:utf-8
 
+require "rss"
+require "yaml"
+
+require "httpclient"
 require "sinatra"
 require "hamlit"
 
 get "/" do
-    haml :index
+    data = YAML.load(File.read('list.yaml'))
+
+    data.each do |row|
+        client = HTTPClient.new
+
+        res = client.get(row["url"])
+        feed = RSS::Parser.parse(res.body)
+        @feed = feed.items
+    end
+
+    haml :index 
 end
 
-get "/reload" do
-end
-
-get "/feed/:id" do
-end
